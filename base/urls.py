4a23b380350a8e8c3a2ef03e7011c711f3af5ba8
Nithishrexson <157978285+Nithishrexson@users.noreply.github.com>
2026-05-22 15:38:05 +0530

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('home/',home,name ='home'),
+    path('',home,name ='home'),
     path('event/',event,name ='event'),
     path('news/',news,name ='news'),
     path('about/',about,name ='about'),
